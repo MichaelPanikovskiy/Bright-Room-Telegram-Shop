86b7a3f0867b0ae0dbe08e6597631bc47470ea80
s1awisch <144999458+s1awisch@users.noreply.github.com>
2025-02-10 00:18:50 +0300

    Приложение
    Реализован базовый функционал, разработан дизайн

--- BrightRoom/app.py
+++ BrightRoom/app.py
@@ -1,10 +1,26 @@
-from flask import Flask, render_template  
+from flask import Flask, render_template
+from aiogram import Bot, Dispatcher, types
+from aiogram.contrib.fsm_storage.memory import MemoryStorage
+from aiogram.utils import executor
+
+API_TOKEN = '7600134141:AAH093R872jeekPiVP_R9xRmDnX3Z1Jo7dg'
+
+# Создание объектов бота и диспетчера
+bot = Bot(token=API_TOKEN)
+storage = MemoryStorage()
+dp = Dispatcher(bot, storage=storage)  
   
 app = Flask(__name__, template_folder='.')  
   
 @app.route("/")  
 def web():  
     return render_template('index.html')  
+    
+# Обработчик команды /start
+@dp.message_handler(commands=['start'])
+async def send_welcome(message: types.Message):
+    await message.reply("Привет! Я простой бот на aiogram. Чем могу помочь? 😊")
   
 if __name__ == "__main__":  
-    app.run(debug=True, host="0.0.0.0", port='80')  
+    app.run(debug=True, host="0.0.0.0", port='80')
+    executor.start_polling(dp, skip_updates=True)    
--- BrightRoom/bot.py
+++ BrightRoom/bot.py
@@ -14,27 +14,10 @@ logging.basicConfig(level=logging.INFO)
 bot = Bot(os.getenv("TOKEN"))
 dp = Dispatcher()
 
-@dp.message(CommandStart())
-async def start(message: types.Message):
-    webAppInfo = types.WebAppInfo(url="your-webapp-url")
-    builder = ReplyKeyboardBuilder()
-    builder.add(types.KeyboardButton(text='Отправить данные', web_app=webAppInfo))
-    
-    await message.answer(text='Привет!', reply_markup=builder.as_markup())
-    
 # Команда старт
 @dp.message_handler(commands=['start'])
 async def respond_knb(message: types.Message):
     await message.reply("Добро пожаловать в BrightRoom! Откройте для себя наш каталог товаров и найдите идеальное освещение для вашего дома или офиса.")
-
-@dp.message(F.content_type == ContentType.WEB_APP_DATA)
-async def parse_data(message: types.Message):
-    data = json.loads(message.web_app_data.data)
-    await message.answer(f'<b>{data["title"]}</b>\n\n<code>{data["desc"]}</code>\n\n{data["text"]}', parse_mode=ParseMode.HTML)
-
-async def main():
-    await bot.delete_webhook(drop_pending_updates=True)
-    await dp.start_polling(bot)
     
 if __name__ == "__main__":
     asyncio.run(main())
